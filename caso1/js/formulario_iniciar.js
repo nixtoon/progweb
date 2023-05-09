d832910f7f6919d6b04e86f2482782c99f244a00
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-05-08 21:38:22 -0400

    validaciones

--- caso1/js/formulario_iniciar.js
+++ caso1/js/formulario_iniciar.js
@@ -1,24 +1,34 @@
+// la funcion se ejecutará cuando se precione el botón de iniciar sesión 
+// de tipo submit
 $("#main").submit(
-    function(){
+    function () {
+        // capturamos el formulario de registro por medio del id
         var $iniciar = $("#main")
-        $.validator.addMethod("espacios", function(value, element){
+
+        //metodo creado para evitar los espacios en los inputs
+        $.validator.addMethod("espacios", function (value, element) {
             return value == ' ' || value.trim().length != 0
         }, "Espacios no son permitidos");
-        if($iniciar.length){
+
+        // validamos el formulario de registro campo por campo
+        if ($iniciar.length) {
             $iniciar.validate({
-                rules:{
+                // pasamos los parametros de validacion
+                rules: {
                     email: {
                         required: true,
                         email: true
                     },
-                    password:{
+                    password: {
                         required: true,
                         espacios: true,
                         minlength: 5,
                         maxlength: 15
                     }
                 },
-                messages:{
+                // escribimos los mensajes para que aparezcan cuando los campos
+                // sean incorrectos
+                messages: {
                     email: {
                         required: 'Correo es obligatorio',
                     },
@@ -31,6 +41,8 @@ $("#main").submit(
 
             })
         }
+        // evitamos que el formulario se envíe por decfecto al apretar el boton
+        // de iniciar
         event.preventDefault();
     }
 );
--- caso1/js/formulario_registrar.js
+++ caso1/js/formulario_registrar.js
@@ -1,15 +1,22 @@
+// la funcion se ejecutará cuando se precione el botón de registrarse 
+// de tipo submit
 $("#registrar").submit(
     function(){
+        // capturamos el formulario de registro por medio del id
         var $registrar = $("#registrar")
+
+        //metodo creado para evitar los espacios en los inputs
         $.validator.addMethod("espacios", function(value, element){
             return value == ' ' || value.trim().length != 0
         }, "Espacios no son permitidos");
+
+        // validamos el formulario de registro campo por campo
         if($registrar.length){
             $registrar.validate({
                 rules:{
+                    // pasamos los parametros de validacion
                     nombre: {
                         required: true,
-                        text: true,
                         espacios: true,
                         minlength: 4,
                         maxlength: 20,
@@ -33,9 +40,12 @@ $("#registrar").submit(
                     }
 
                 },
+
+                // escribimos los mensajes para que aparezcan cuando los campos
+                // sean incorrectos
                 messages:{
                     nombre: {
-                        required: 'nombre es obligatorio',
+                        required: 'Nombre es obligatorio',
                         minlength:'debe tener un mínimo de 4 caracteres',
                         maxlength:'debe tener un máximo de 20 caracteres',
                         text:'Solo se aceptan letras',
@@ -56,6 +66,8 @@ $("#registrar").submit(
 
             })
         }
+        // evitamos que el formulario se envíe por decfecto al apretar el boton
+        // de registrarse
         event.preventDefault();
     }
 );
