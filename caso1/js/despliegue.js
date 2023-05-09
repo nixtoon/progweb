463796428ddec5b416d8536f37dd527a8669a9b4
se4arriagadaa <84482238+se4arriagadaa@users.noreply.github.com>
2023-05-08 21:39:43 -0400

    dfjhk

---
+++ caso1/js/despliegue.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+        $("#panel").slideUp("slow");
+    });
+});
